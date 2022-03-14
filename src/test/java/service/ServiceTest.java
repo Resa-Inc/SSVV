de4f30b90a4c08018392999438112707597c79de
iulia <iuliarevnic@yahoo.com>
2022-03-14 09:33:11 +0200

    test id not in database

--- src/test/java/service/ServiceTest.java
+++ src/test/java/service/ServiceTest.java
@@ -1,6 +1,7 @@
 package service;
 
 import domain.Student;
+import org.junit.jupiter.api.Test;
 import repository.NotaXMLRepo;
 import repository.StudentXMLRepo;
 import repository.TemaXMLRepo;
@@ -32,5 +33,21 @@ class ServiceTest {
         assert !addStudent.getEmail().equals(newStudent.getEmail());
         assert addStudent.getGrupa() != (newStudent.getGrupa());
 
+    }
+
+    @org.junit.jupiter.api.Test
+    void addStudent_ID_Not_In_Database() {
+        Student newStudent = new Student("2", "student", 931, "student@yahoo.com");
+        StudentXMLRepo studentFileRepository = new StudentXMLRepo("fisiere/Studenti.xml");
+        StudentValidator studentValidator = new StudentValidator();
+        TemaXMLRepo temaFileRepo = new TemaXMLRepo("fisiere/Teme.xml");
+        TemaValidator temaValidator = new TemaValidator();
+        NotaXMLRepo notaFileRepo = new NotaXMLRepo("fisiere/Note.xml");
+        NotaValidator notaValidator = new NotaValidator(studentFileRepository, temaFileRepo);
+        Service service = new Service(studentFileRepository, studentValidator, temaFileRepo, temaValidator, notaFileRepo, notaValidator);
+
+        Student addStudent = service.addStudent(newStudent);
+
+        assert addStudent == null;
     }
 }
